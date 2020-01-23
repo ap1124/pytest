a322cefddaa96af7df1dca992698c8097be4dfa0
ap1124 <patelabhi1124@gmail.com>
2020-01-23 17:55:28 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,12 +1,14 @@
-import random
-
 listt = list(input("Enter the list: ").split())
+#take list from user
 
 list1 = [int(i) for i in listt]
+#change each element to int
 
 sval = int(input("Enter shift value: "))
-i = [*range(len(list1))]
+#take rotation value
 
+i = [*range(len(list1))]
 list2 = list(map(lambda i : list1[(i+sval)%len(list1)] , i ))
+#perform and store rotated list
 
 print(f"Original list: {list1}\nOutput list: {list2}")
